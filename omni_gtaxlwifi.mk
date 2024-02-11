b9847fd01d8afe3970051bf700f57d4555e566ec
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-02-11 23:38:11 +0200

    Update omni_gtaxlwifi.mk

--- omni_gtaxlwifi.mk
+++ omni_gtaxlwifi.mk
@@ -12,17 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from gtaxllte device
-$(call inherit-product, device/samsung/gtaxllte/device.mk)
+$(call inherit-product, device/samsung/gtaxlwifi/device.mk)
 
-PRODUCT_DEVICE := gtaxllte
-PRODUCT_NAME := omni_gtaxllte
+PRODUCT_DEVICE := gtaxlwifi
+PRODUCT_NAME := omni_gtaxlwifi
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T585
+PRODUCT_MODEL := SM-T580
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="gtaxlltexx-user 8.1.0 M1AJQ T585XXU7CUH1 release-keys"
-
-BUILD_FINGERPRINT := samsung/gtaxlltexx/gtaxllte:8.1.0/M1AJQ/T585XXU7CUH1:user/release-keys
