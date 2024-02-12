4d285fe96aa742c5852addf328f721afa29447ea
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-02-12 02:07:19 +0200

    Update omni_gtaxlwifi.mk

--- omni_gtaxlwifi.mk
+++ omni_gtaxlwifi.mk
@@ -7,12 +7,8 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
+$(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from gtaxllte device
-$(call inherit-product, device/samsung/gtaxlwifi/device.mk)
 
 PRODUCT_DEVICE := gtaxlwifi
 PRODUCT_NAME := omni_gtaxlwifi
